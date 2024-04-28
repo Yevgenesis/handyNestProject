0a7fd7669e1af4bbf2924d0f362907692a5cdc19
isninochka <ninaisaeva0919@gmail.com>
2024-04-28 21:01:39 +0200

    created handydb

--- src/main/java/codezilla/handynestproject/HandyNestProjectApplication.java
+++ src/main/java/codezilla/handynestproject/HandyNestProjectApplication.java
@@ -2,8 +2,11 @@ package codezilla.handynestproject;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+
 public class HandyNestProjectApplication {
 
     public static void main(String[] args) {
--- src/main/java/codezilla/handynestproject/service/impl/UserServiceImpl.java
+++ src/main/java/codezilla/handynestproject/service/impl/UserServiceImpl.java
@@ -1,24 +1,24 @@
-//package codezilla.handynestproject.service.impl;
-//
-//import codezilla.handynestproject.model.entity.UserInfo;
-//import codezilla.handynestproject.repository.UserRepository;
-//import codezilla.handynestproject.service.UserService;
-//import org.springframework.stereotype.Service;
-//
-//import java.util.List;
-//
-//@Service
-//public class UserServiceImpl implements UserService {
-//
-//    private final UserRepository userRepository;
-//
-//    public UserServiceImpl(UserRepository userRepository) {
-//        this.userRepository = userRepository;
-//    }
-//
-//
-//    @Override
-//    public List<UserInfo> getUsers() {
-//        return userRepository.findAll();
-//    }
-//}
+package codezilla.handynestproject.service.impl;
+
+import codezilla.handynestproject.model.entity.UserInfo;
+import codezilla.handynestproject.repository.UserRepository;
+import codezilla.handynestproject.service.UserService;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserServiceImpl implements UserService {
+
+    private final UserRepository userRepository;
+
+    public UserServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+
+    @Override
+    public List<UserInfo> getUsers() {
+        return userRepository.findAll();
+    }
+}
