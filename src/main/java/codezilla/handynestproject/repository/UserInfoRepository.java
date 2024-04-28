d22b33bc3dd9dd598bef6f269bc9f89b7815dc26
isninochka <ninaisaeva0919@gmail.com>
2024-04-28 20:45:05 +0200

    deleted UUID, not connected with PostgreSql. Updated @OneToMany

--- src/main/java/codezilla/handynestproject/repository/UserInfoRepository.java
+++ src/main/java/codezilla/handynestproject/repository/UserInfoRepository.java
@@ -1,11 +1,10 @@
 package codezilla.handynestproject.repository;
 
-import codezilla.handynestproject.model.entity.Performer;
 import codezilla.handynestproject.model.entity.UserInfo;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface UserRepository extends JpaRepository<UserInfo, Long> {
+public interface UserInfoRepository extends JpaRepository<UserInfo, Long> {
 
 }
