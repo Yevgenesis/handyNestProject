dc2729c34737c6ccc7f96c4fc5064bfaa42ef217
yevhenii.ashurov <yevhenii.ashurov@occe>
2024-05-03 01:54:27 +0200

    fix WorkingTimeRepository

--- src/main/java/codezilla/handynestproject/repository/WorkingTimeRepository.java
+++ src/main/java/codezilla/handynestproject/repository/WorkingTimeRepository.java
@@ -8,7 +8,7 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface WorkingTimeRepository extends JpaRepository<WorkingTime, Long> {
 
-    WorkingTime getById(Long workingTimeId);
+    WorkingTime getWorkingTimeById(Long workingTimeId);
 
 
 }
