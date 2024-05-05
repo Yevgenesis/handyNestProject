69e3b773af0337feff3cc9a7b9be2d24516f097f
isninochka <ninaisaeva0919@gmail.com>
2024-05-05 16:26:03 +0200

    task mapper, Postman

--- src/main/java/codezilla/handynestproject/controller/TaskController.java
+++ src/main/java/codezilla/handynestproject/controller/TaskController.java
@@ -4,10 +4,15 @@ import codezilla.handynestproject.dto.task.TaskRequestDto;
 import codezilla.handynestproject.dto.task.TaskResponseDto;
 import codezilla.handynestproject.dto.task.TaskUpdateRequestDto;
 import codezilla.handynestproject.mapper.TaskMapper;
-import codezilla.handynestproject.model.entity.Task;
 import codezilla.handynestproject.service.TaskService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
@@ -19,12 +24,12 @@ public class TaskController {
     private final TaskService taskService;
     private final TaskMapper taskMapper;
 
+
     @GetMapping
     public List<TaskResponseDto> getAllTasks() {
          return taskMapper.toTaskResponseDtoList
                 (taskService.getAllTasks());
     }
-
 
 
     @GetMapping("/{id}")
@@ -37,7 +42,7 @@ public class TaskController {
         return taskMapper.toTaskResponseDto(taskService.createTask(taskRequestDto));
     }
 
-    @PostMapping("/update/{id}")
+    @PutMapping("/update/{id}")
     public TaskResponseDto updateTask(@PathVariable("id") Long id, @RequestBody TaskUpdateRequestDto taskUpdateRequestDto) {
         return taskMapper.toTaskResponseDto(taskService.updateTask(taskUpdateRequestDto));
     }
--- src/main/java/codezilla/handynestproject/mapper/UserMapper.java
+++ src/main/java/codezilla/handynestproject/mapper/UserMapper.java
@@ -11,6 +11,7 @@ import java.util.List;
 public interface UserMapper {
 
     UserResponseDto userToDto(User user);
-
     List<UserResponseDto> usersToListDto(List<User> users);
+
+
 }
