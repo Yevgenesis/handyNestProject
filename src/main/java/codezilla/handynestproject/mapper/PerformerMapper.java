73975a82c4e7db2f8f6089610b13fb2583219971
yevhenii.ashurov <yevhenii.ashurov@occe>
2024-05-05 22:12:27 +0200

    fix sql

--- src/main/java/codezilla/handynestproject/mapper/PerformerMapper.java
+++ src/main/java/codezilla/handynestproject/mapper/PerformerMapper.java
@@ -3,15 +3,19 @@ package codezilla.handynestproject.mapper;
 
 import codezilla.handynestproject.dto.performer.PerformerResponseDto;
 import codezilla.handynestproject.model.entity.Performer;
-import codezilla.handynestproject.model.entity.User;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 import java.util.List;
 
 @Mapper(componentModel = "spring")
 public interface PerformerMapper {
 
-    PerformerResponseDto performerToDto(User user);
+    @Mapping(source = "user.firstName", target = "firstName")
+    @Mapping(source = "user.lastName", target = "lastName")
+    @Mapping(source = "performer.created_on", target = "createdOn")
+    @Mapping(source = "performer.updated_on", target = "updatedOn")
+    PerformerResponseDto performerToDto(Performer performer);
 
     List<PerformerResponseDto> performersToListDto(List<Performer> performers);
 }
