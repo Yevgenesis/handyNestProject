605a2af478409de9927b662de1e63944f05217fe
yevhenii.ashurov <yevhenii.ashurov@occe>
2024-05-06 22:42:38 +0200

    add Performer to Task

--- src/main/java/codezilla/handynestproject/dto/task/TaskResponseDto.java
+++ src/main/java/codezilla/handynestproject/dto/task/TaskResponseDto.java
@@ -1,11 +1,11 @@
 package codezilla.handynestproject.dto.task;//package codezilla.hendynestproject.model.entity;
 
+import codezilla.handynestproject.dto.category.CategoryTitleDto;
+import codezilla.handynestproject.dto.performer.PerformerResponseDto;
+import codezilla.handynestproject.dto.user.UserResponseDto;
 import codezilla.handynestproject.model.entity.Address;
-import codezilla.handynestproject.model.entity.Category;
-import codezilla.handynestproject.model.entity.User;
 import codezilla.handynestproject.model.entity.WorkingTime;
 import codezilla.handynestproject.model.enums.TaskStatus;
-import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Data;
 
 // ToDo проверить (Тестовый DTO)
@@ -19,7 +19,8 @@ public class TaskResponseDto {
     private Address address;
     private TaskStatus taskStatus;
     private WorkingTime workingTime;
-    private Category category;
-    private User user;
+    private CategoryTitleDto category;
+    private UserResponseDto user;
+    private PerformerResponseDto performer;
     private boolean isPublish;
    }
