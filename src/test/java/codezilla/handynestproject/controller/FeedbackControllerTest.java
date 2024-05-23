aabbacf07688ef3838f3fe7c4f401043010a8a1b
isninochka <ninaisaeva0919@gmail.com>
2024-05-23 11:35:15 +0200

    CategoryControllerTest, not all FeedbackControllerTest

--- src/test/java/codezilla/handynestproject/controller/FeedbackControllerTest.java
+++ src/test/java/codezilla/handynestproject/controller/FeedbackControllerTest.java
@@ -22,7 +22,6 @@ import static codezilla.handynestproject.service.TestData.FEEDBACK_RESPONSE_DTO1
 import static codezilla.handynestproject.service.TestData.FEEDBACK_RESPONSE_DTO2;
 import static codezilla.handynestproject.service.TestData.FEEDBACK_RESPONSE_DTO3;
 import static codezilla.handynestproject.service.TestData.FEEDBACK_RESPONSE_DTO5;
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
