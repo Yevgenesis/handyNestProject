4e2e5abb6390dfa415e631325af25e908dfcb8eb
yevhenii.ashurov <yevhenii.ashurov@occe>
2024-06-08 02:42:04 +0200

    add AttachmentTest

--- src/main/java/codezilla/handynestproject/controller/AttachmentController.java
+++ src/main/java/codezilla/handynestproject/controller/AttachmentController.java
@@ -12,14 +12,7 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -43,7 +36,7 @@ public class AttachmentController {
             @ApiResponse(responseCode = "200", description = "Successfully created"),
             @ApiResponse(responseCode = "404", description = "Attachment not created")
     })
-    @PreAuthorize("hasAnyAuthority('USER','PERFORMER','ADMIN')")
+    @PreAuthorize("hasAnyAuthority('PERFORMER','ADMIN')")
     @PostMapping
     public AttachmentDto create(@RequestBody @Valid AttachmentRequestDto attachmentRequestDto) {
         log.info("Create Attachment Request: {}", attachmentRequestDto);
@@ -78,7 +71,7 @@ public class AttachmentController {
             @ApiResponse(responseCode = "200", description = "Successfully retrieved"),
             @ApiResponse(responseCode = "404", description = "Attachment not found")
     })
-    @PreAuthorize("hasAnyAuthority('USER','PERFORMER','ADMIN')")
+    @PreAuthorize("hasAnyAuthority('ADMIN')")
     @GetMapping("/{id}")
     public AttachmentDto findById(@PathVariable Long id) {
         log.info("Find Attachment by id: {}", id);
--- src/test/java/codezilla/handynestproject/controller/CategoryControllerTest.java
+++ src/test/java/codezilla/handynestproject/controller/CategoryControllerTest.java
@@ -1,15 +1,14 @@
 package codezilla.handynestproject.controller;
 
 import codezilla.handynestproject.HandyNestProjectApplication;
-import codezilla.handynestproject.service.CategoryService;
 import codezilla.handynestproject.util.TestDatabaseConfig;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.SneakyThrows;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import org.testcontainers.junit.jupiter.Testcontainers;
@@ -21,18 +20,12 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @SpringBootTest
 @Testcontainers
 @AutoConfigureMockMvc
+@ActiveProfiles("test")
 @ContextConfiguration(classes = {TestDatabaseConfig.class, HandyNestProjectApplication.class})
 class CategoryControllerTest {
 
-
-    @Autowired
-    private CategoryService categoryService;
-
     @Autowired
     private MockMvc mockMvc;
-
-    @Autowired
-    private ObjectMapper objectMapper;
 
 
     @Test
